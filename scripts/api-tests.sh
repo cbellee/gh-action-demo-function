889e9337423cfc56744df140ed90f0498c35995d
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2023-11-28 10:50:23 +1100

    bump...

--- scripts/api-tests.sh
+++ scripts/api-tests.sh
@@ -8,3 +8,4 @@ curl https://dev-gh-action-demo-function-repysa6fou2tw.azurewebsites.net/api/ale
 curl -X PUT https://dev-gh-action-demo-function-repysa6fou2tw.azurewebsites.net/api/alertlistchannels?code=cNmiwRmRpLcW3p1HwNkFUyONsDXPoGsu8kzgZJragd4GAzFu9kjF1Q== \
     -H 'Content-Type: application/json' \
     -d '[{"alertListId": "1", "alertListName": "Alert List 1","teamsChannelId": "100","teamsChannelName": "Teams Channel 100"},{"alertListId": "2", "alertListName": "Alert List 2","teamsChannelId": "200","teamsChannelName": "Teams Channel 200"},{"alertListId": "3", "alertListName": "Alert List 3","teamsChannelId": "300","teamsChannelName": "Teams Channel 300"},{"alertListId": "4", "alertListName": "Alert List 4","teamsChannelId": "400","teamsChannelName": "Teams Channel 400"}]'
+
